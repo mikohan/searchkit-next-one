ff1e876bf61db16707a5353f0abae4c0ad3d24c0
mikohan <angara99@gmail.com>
2021-02-15 16:57:41 +0300

    Messing around

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,4 +1,23 @@
 import { ApolloServer, gql } from 'apollo-server-micro';
+import { MultiMatchQuery, SearchkitSchema } from '@searchkit/schema';
+
+const searchkitConfig = {
+  host: 'http://localhost:9200',
+  index: 'movies',
+  hits: {
+    fields: [],
+  },
+  query: new MultiMatchQuery({ fields: [] }),
+  facets: [],
+};
+
+// Returns SDL + Resolvers for searchkit, based on the Searchkit config
+const { typeDefs, withSearchkitResolvers, context } = SearchkitSchema({
+  config: searchkitConfig, // searchkit configuration
+  typeName: 'ResultSet', // type name for Searchkit Root
+  hitTypeName: 'ResultHit', // type name for each search result
+  addToQueryType: true, // When true, adds a field called results to Query type
+});
 
 export const config = {
   api: {
@@ -6,24 +25,30 @@ export const config = {
   },
 };
 
-const typeDefs = [
-  gql`
-    type Query {
-      root: String
-    }
-
-    type Mutation {
-      root: String
-    }
-  `,
-];
-
 const server = new ApolloServer({
-  typeDefs,
-  resolvers: {},
+  typeDefs: [
+    gql`
+      type Query {
+        root: String
+      }
+
+      type HitFields {
+        root: String
+      }
+
+      type ResultHit implements SKHit {
+        id: ID!
+        fields: HitFields
+      }
+    `,
+    ...typeDefs,
+  ],
+  resolvers: withSearchkitResolvers({}),
   introspection: true,
   playground: true,
-  context: {},
+  context: {
+    ...context,
+  },
 });
 
 export default server.createHandler({ path: '/api/graphql' });
